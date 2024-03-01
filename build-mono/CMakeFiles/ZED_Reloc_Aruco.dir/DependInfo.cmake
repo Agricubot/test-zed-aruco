8199681bcc91fbd66de89672c3ab0479766e17a9
ghbot <your_email@example.com>
2024-03-01 18:59:37 +0100

    Generacion de ejecutables

---
+++ build-mono/CMakeFiles/ZED_Reloc_Aruco.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ghbot/ZED/zed-aruco/mono/src/aruco.cpp" "/home/ghbot/ZED/zed-aruco/build-mono/CMakeFiles/ZED_Reloc_Aruco.dir/src/aruco.cpp.o"
+  "/home/ghbot/ZED/zed-aruco/mono/src/main.cpp" "/home/ghbot/ZED/zed-aruco/build-mono/CMakeFiles/ZED_Reloc_Aruco.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/zed/include"
+  "/home/ghbot/ZED/zed-aruco/mono/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-multi/CMakeFiles/ZED_Multi_Reloc_Aruco.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_Multi_Reloc_Aruco.dir/src/GLViewer.cpp.o"
+  "CMakeFiles/ZED_Multi_Reloc_Aruco.dir/src/aruco.cpp.o"
+  "CMakeFiles/ZED_Multi_Reloc_Aruco.dir/src/main.cpp.o"
+  "ZED_Multi_Reloc_Aruco"
+  "ZED_Multi_Reloc_Aruco.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_Multi_Reloc_Aruco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
